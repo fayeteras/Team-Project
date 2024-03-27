16041298c95993e50e0e9138a003dd798ad14419
savnimaheshwari <143203323+savnimaheshwari@users.noreply.github.com>
2024-03-27 17:43:09 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,1 +1,14 @@
+import java.util.List;
+
+public interface UserInterface {
+    void createUser(String username, String password);
+    boolean authenticateUser(String username, String password);
+    void addFriend(String username);
+    void removeFriend(String username);
+    void blockUser(String username);
+    void unblockUser(String username); 
+    String[] getFriends();
+    String[] getBlockedUsers();
+}
+
 
