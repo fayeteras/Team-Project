af4c8c62837f1acc06d79e430640bfd818533ac4
savnimaheshwari <143203323+savnimaheshwari@users.noreply.github.com>
2024-03-27 19:06:43 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,1 +1,79 @@
+public class User implements UserInterface {
+    private String username;
+    private String password;
+    private String[] friends;
+    private boolean[] blockedUsers;
+    private int friendCount;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.friends = new String[100];
+        this.blockedUsers = new boolean[100];
+        this.friendCount = 0;
+    }
+
+
+    public void createUser(String username, String password) {
+//implement
+    }
+
+
+    public void searchUser(String username) {
+        // Implementation to search for a user
+    }
+
+
+    public boolean authenticateUser(String username, String password) {
+        return this.username.equals(username) && this.password.equals(password);
+    }
+    
+    public void addFriend(String friendUsername) {
+        if (friendCount < friends.length) {
+            friends[friendCount++] = friendUsername;
+        } else {
+            System.out.println("Cannot add more friends. Friend limit reached.");
+        }
+    }
+
+
+    public void removeFriend(String friendUsername) {
+//implement
+        }
+
+
+    public void blockUser(String username) {
+      //implement
+    }
+
+
+    public void unblockUser(String username) {
+//implement
+    }
+    // method to save the information to all_users.txt
+  //public void saveToFile(String fileName) {
+        //try (PrintWriter writer = new PrintWriter(new FileWriter(fileName, true))) {
+           // writer.println(username + "|" + password + "|" + String.join("|", friends) + "|" + String.join("|", blockedUsers));
+       // } catch (IOException e) {
+         //   e.printStackTrace();
+        //}
+   // }
+    // other methods
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String[] getFriends() {
+        return friends;
+    }
+
+    public boolean[] getBlockedUsers() {
+        return blockedUsers;
+    }
+
+    public int getFriendCount() {
+        return friendCount;
+    }
+}
 
