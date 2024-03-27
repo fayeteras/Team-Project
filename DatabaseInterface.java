cfc67b344cc6f17ab0ecaa605a3e94f1ec04e035
savnimaheshwari <143203323+savnimaheshwari@users.noreply.github.com>
2024-03-27 18:28:52 -0400

    Update DatabaseInterface.java

--- DatabaseInterface.java
+++ DatabaseInterface.java
@@ -1,1 +1,39 @@
+public interface DatabaseInterface {
 
+        void createUser(String username, String password);
+
+        void searchUser(String username);
+
+        boolean authenticateUser(String username, String password);
+
+        void addFriend(String username);
+
+        void removeFriend(String username);
+
+        void blockUser(String username);
+
+        void unblockUser(String username);
+
+        void makePost(String content);
+
+        Post[] getFeed(); // displaying all friends feed
+
+        void upvotePost(Post post); 
+
+        void addComment(Post post, Comment comment); // Add comment to a post
+
+        void deleteComment(Post post, Comment comment); // Delete comment from a post
+
+        void downvotePost(Post post);
+
+        void upvoteComment(Comment comment);
+
+        void downvoteComment(Comment comment);
+
+        void hidePost(Post post);
+
+        void deletePost(Post post);
+  
+//some methods might require or work better with boolean
+
+    }
