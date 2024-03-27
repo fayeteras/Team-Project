eeddc802b13a0c43983017922f2fd561bb2d744c
savnimaheshwari <143203323+savnimaheshwari@users.noreply.github.com>
2024-03-27 19:17:13 -0400

    Update DatabaseInterface.java

--- DatabaseInterface.java
+++ DatabaseInterface.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
 
+//not sure about this but this is one way in which it can be implemented
 public interface DatabaseInterface {
-    void saveUserInfo(String userInfo) throws IOException;
+    void saveUserInfo(String userInfo) throws IOException; //all_users.txt
 
-    void savePostInfo(String postInfo) throws IOException;
+    void savePostInfo(String postInfo) throws IOException; //userPosts.txt
 
-    void saveCommentInfo(String commentInfo) throws IOException;
+    void saveCommentInfo(String commentInfo) throws IOException; //userComments.txt
 }
