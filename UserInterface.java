6cb6852fa54421d557ca4891a5d19b30dddfb60f
Sean VandenBussche <165090862+Seanski05@users.noreply.github.com>
2024-03-27 23:17:17 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,19 +1,47 @@
 public interface UserInterface {
-    void createUser(String username, String password);
+    //(Sean) the getters. If there are more variables you think
+    //are needed for the class, don't forget to add the getter.
+    public String getUsername();
+    public String getPassword();
+    public String[] getFriendList();
+    public String[] getBlockList();
 
-    void searchUser(String username);
+    //(Sean) **FILE USED**
+    //creates a new user by appending the allUsers file
+    //to include the new user.
+    public void createUser();
 
-    boolean authenticateUser(String username, String password);
+    //(Sean) **FILE USED**
+    //Uses the username parameter to compare to every entry in the
+    //allUsers file, returning the entire user string if found.
+    //Of course the entire user string is too much, but providing too
+    //much is better than too little.
+    //OPTIONAL COMPLEXITY - HW-11 showed how to fetch a line based on a
+    //substring of its content. Maybe this method doesn't require a full username,
+    //rather a substring.. like "fay" grabs "fayeteras"?
+    //Edit: I'm so awesome that I implemented this complexity
+    //Please don't duplicate it on your HW-11 or I will find you.
+    public String[] searchUsers(String searchText);
 
-    void addFriend(String username);
+    //Returns true or false based on whether or not the username and
+    //password parameters match the values of the User object
+    public boolean authenticateUser(String username, String password);
 
-    void removeFriend(String username);
+    //(Sean) adds a friend String to the friendList array in the user class
+    public void addFriend(String friendUsername);
 
-    void blockUser(String username);
+    //(Sean) removes a friend string from the friendList.
+    public void removeFriend(String friendUsername);
 
-    void unblockUser(String username);
+    //(Sean) Adds a user String to the blockList array in the user class
+    //This functions almost the exact same way as addFriend() btw
+    public void blockUser(String friendUsername);
 
-    Post[] getFeed(); // displaying all friends feed
+    //(Sean) Removes a user string from the blockList.
+    //This functions almost the exact same way as removeFriend() btw
+    public void unblockUser(String friendUsername);
 
-
+    //(Sean) Stringafies the fields of the User class
+    //Formatting can be seen in allUsers.txt
+    public String toString();
 }
