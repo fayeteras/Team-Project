6f2aef869a71dc704bcf2fcd891d2a58e960fa8c
noahwithers <165091315+noahwithers@users.noreply.github.com>
2024-03-28 17:42:58 -0400

    Update Database.java

--- Database.java
+++ Database.java
@@ -6,15 +6,66 @@ public class Database implements DatabaseInterface{
 	}
 
 	public boolean addUser(User user) {
-		//append to it
+		try {
+			File f = new File(filename);
+			FileOutputStream fos = new FileWriter(f);
+			FileWriter fw = new FileWriter(fos);
+	
+			fw.println(user.getUsername() + ", " + user.getPassword());
+	
+			fw.close();
+			return true;
+		} catch (Exception ex) {
+			return false;
+		}
 	}
 
 	public boolean userExists(String username) {
-		//return if the username is in the user list
+		try {
+			File f = new File(filename);
+			FileReader fr = new FileReader(f);
+			BufferedReader bfr = new BufferedReader(fr);
+
+			String line;
+			boolean found = false;
+			while(true) {
+				line = bfr.nextLine();
+				if (line == null)
+					break;
+
+				if (line.substring(0, line.indexOf(",")).equals(username)) {
+					found = true;
+					break;
+				}
+			}
+			bfr.close();
+			return found;
+		} catch (Exception ex) {
+			return false;
+		}
 	}
 
 	public String getPassword(String username) {
-		//get the password for the username given
+		try {
+			File f = new File(filename);
+			FileReader fr = new FileReader(f);
+			BufferedReader bfr = new BufferedReader(fr);
+
+			String line;
+			while(true) {
+				line = bfr.nextLine();
+				if (line == null)
+					break;
+
+				if (line.substring(0, line.indexOf(",")).equals(username)) {
+					return line.substring(line.indexOf("," + 1));
+				}
+			}
+			bfr.close();
+			return null;
+		} catch (Exception ex) {
+			return null;
+		}
 	}
 	
 }
