2d13695c0eca3b8b26e08db76aa5690995cf9b16
noahwithers <165091315+noahwithers@users.noreply.github.com>
2024-03-28 17:56:06 -0400

    Update Comment.java

--- Comment.java
+++ Comment.java
@@ -1,9 +1,14 @@
+//(Noah) marked as i made this 
 public class Comment extends Post implements CommentInterface {
     private final Post parent;
 
     public Comment(String username, String text, Post parent) {
         super(username, text);
         this.parent = parent;
+    }
+
+    public Post getParent() {
+        return parent;
     }
 }
 
