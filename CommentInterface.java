e194c5ef01f00d43f5cb6af67e34107729abe281
Sean VandenBussche <165090862+Seanski05@users.noreply.github.com>
2024-03-27 23:22:19 -0400

    Update CommentInterface.java

--- CommentInterface.java
+++ CommentInterface.java
@@ -1,9 +1,14 @@
 public interface CommentInterface {
-    void addComment(Post post, Comment comment); // Add comment to a post
+    //(Savni) Takes a comment from the user and adds it to the post specified by the user
+    public void addComment(Post post, Comment comment);
+    
+    //(Savni) Takes a comment from the user and deletes it from the post specified by the user
+    //Be aware that only a post owner or comment owner can do this
+    public void deleteComment(Post post, Comment comment);
+    
+    //(Savni) Takes a comment and increments the likes of it
+    public void likeComment(Comment comment);
 
-    void deleteComment(Post post, Comment comment); // Delete comment from a post
-    void upvoteComment(Comment comment);
-
-    void downvoteComment(Comment comment);
-
+    //(Savni) Takes a comment and increments the dislikes of it
+    public void dislikeComment(Comment comment);
 }
