a79c83de4eea4891c8e902ac877fcb32a55da4bb
Sean VandenBussche <165090862+Seanski05@users.noreply.github.com>
2024-03-27 23:25:56 -0400

    Update PostInterface.java

--- PostInterface.java
+++ PostInterface.java
@@ -1,12 +1,19 @@
 public interface PostInterface {
-    void makePost(String content); //createPost
+    //(Savni) Creates a post that contains a string of content
+    public void makePost(String content);
 
-    void upvotePost(Post post);
-    
-    void downvotePost(Post post);
-    void hidePost(Post post);
+    //(Savni) Takes a post and increments the like total of it
+    public void likePost(Post post);
 
-    void deletePost(Post post);
+    //(Savni) Takes a post and increments the dislike total of it
+    public void downvotePost(Post post);
+
+    //(Savni) Takes a post and hides it from the user's feed
+    public void hidePost(Post post);
+
+    //(Savni) Takes a post and deletes it
+    //Note that this should only be done by the post owner
+    public void deletePost(Post post);
 
 
 }
