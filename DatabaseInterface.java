4b59788eef637dfb92b4dffb8461b28d348bdd27
Sean VandenBussche <165090862+Seanski05@users.noreply.github.com>
2024-03-27 21:59:37 -0400

    Update DatabaseInterface.java

--- DatabaseInterface.java
+++ DatabaseInterface.java
@@ -1,10 +1,28 @@
-import java.io.IOException;
+import java.io.*;
 
-//not sure about this but this is one way in which it can be implemented
+//(Savni) not sure about this but this is one way in which it can be implemented
+//(Sean) a roadblock that may emerge is that we might be appending our input files in
+//these methods, which might be bad. One large output file may be needed.
 public interface DatabaseInterface {
-    void saveUserInfo(String userInfo) throws IOException; //all_users.txt
+    //(Savni) The file used for this method is allUsers.txt
+    //(Sean) this method will save all of the user's info to the 
+    //allUsers file. Be sure to append the user's info if it already existed.
+    public void saveUserInfo(String userInfo) throws IOException;
+    
+    //(Savni) The file used for this method is userPosts.txt
+    //(Sean) this method will save all of the posts to the 
+    //userPosts file. Be sure to append the post if it already existed.
+    public void savePostInfo(String postInfo) throws IOException;
+    
+    //(Savni) The file used for this method is userComments.txt
+    //(Sean) this method will save all of the comments to the 
+    //userComments file. Be sure to append the comment if it already existed.
+    public void saveCommentInfo(String commentInfo) throws IOException;
 
-    void savePostInfo(String postInfo) throws IOException; //userPosts.txt
-
-    void saveCommentInfo(String commentInfo) throws IOException; //userComments.txt
+    //(Sean) This method comes with more assumptions than I can count on my fingers.
+    //Unless the database contains an array of Users, an array of Posts, and an
+    //array of Comments in which each file is read into, there will need to be a lot of 
+    //file reading from multiple files to provide the feed for the user. Hope I'm wrong.
+    //I put this in the database file, as I assume it fits here the most.
+    public Post[] getFeed();
 }
