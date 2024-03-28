b3842d9c08cc05c867489d4e51a7a15c6f354038
noahwithers <165091315+noahwithers@users.noreply.github.com>
2024-03-28 17:48:46 -0400

    Update DatabaseInterface.java

--- DatabaseInterface.java
+++ DatabaseInterface.java
@@ -7,22 +7,34 @@ public interface DatabaseInterface {
     //(Savni) The file used for this method is allUsers.txt
     //(Sean) this method will save all of the user's info to the 
     //allUsers file. Be sure to append the user's info if it already existed.
-    public void saveUserInfo(String userInfo) throws IOException;
+    //(Noah) commented. perish.
+    //public void saveUserInfo(String userInfo) throws IOException;
     
     //(Savni) The file used for this method is userPosts.txt
     //(Sean) this method will save all of the posts to the 
     //userPosts file. Be sure to append the post if it already existed.
-    public void savePostInfo(String postInfo) throws IOException;
+    //Posts do not go in the database. commented.
+    //public void savePostInfo(String postInfo) throws IOException;
     
     //(Savni) The file used for this method is userComments.txt
     //(Sean) this method will save all of the comments to the 
     //userComments file. Be sure to append the comment if it already existed.
-    public void saveCommentInfo(String commentInfo) throws IOException;
+    //(Noah) Comments do not go in the database. commented.
+    //public void saveCommentInfo(String commentInfo) throws IOException;
 
     //(Sean) This method comes with more assumptions than I can count on my fingers.
     //Unless the database contains an array of Users, an array of Posts, and an
     //array of Comments in which each file is read into, there will need to be a lot of 
     //file reading from multiple files to provide the feed for the user. Hope I'm wrong.
     //I put this in the database file, as I assume it fits here the most.
-    public Post[] getFeed();
+    //(Noah) It does not. commented out.
+    //public Post[] getFeed();
+
+    //(Noah) I made the database file. so i'm adding the methods that are in there.
+
+    public boolean addUser(User user);
+
+    public boolean userExists(String username);
+
+    public String getPassword(String username);
 }
