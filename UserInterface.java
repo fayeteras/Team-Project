fedd662db8cdeab9808c054b1d123d67fbdad62a
fayeteras <165090628+fayeteras@users.noreply.github.com>
2024-03-29 08:49:27 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -1,15 +1,20 @@
+import java.io.File;
+import java.util.ArrayList;
+
 public interface UserInterface {
     //(Sean) the getters. If there are more variables you think
     //are needed for the class, don't forget to add the getter.
     public String getUsername();
-    public String getPassword();
-    public String[] getFriendList();
-    public String[] getBlockList();
+    //(Faye) shouldn't need password if it's stored in Database
+//    public String getPassword();
+    public ArrayList<String> getFriendList();
+    public ArrayList<String> getBlockList();
 
     //(Sean) **FILE USED**
     //creates a new user by appending the allUsers file
     //to include the new user.
-    public void createUser();
+    //(Faye) moved to database
+//    public void createUser();
 
     //(Sean) **FILE USED**
     //Uses the username parameter to compare to every entry in the
@@ -21,27 +26,31 @@ public interface UserInterface {
     //rather a substring.. like "fay" grabs "fayeteras"?
     //Edit: I'm so awesome that I implemented this complexity
     //Please don't duplicate it on your HW-11 or I will find you.
-    public String[] searchUsers(String searchText);
-
+    //(Faye) moved to database
+//    public String[] searchUsers(String searchText);
+    
     //Returns true or false based on whether or not the username and
     //password parameters match the values of the User object
-    public boolean authenticateUser(String username, String password);
+//    public boolean authenticateUser(String username, String password);
 
+    //(Faye) I changed all of these to booleans so we can check if there's an error and return an error message
     //(Sean) adds a friend String to the friendList array in the user class
-    public void addFriend(String friendUsername);
+    public boolean addFriend(String friendUsername);
 
     //(Sean) removes a friend string from the friendList.
-    public void removeFriend(String friendUsername);
+    public boolean removeFriend(String friendUsername);
 
     //(Sean) Adds a user String to the blockList array in the user class
     //This functions almost the exact same way as addFriend() btw
-    public void blockUser(String friendUsername);
+    public boolean blockUser(String friendUsername);
 
     //(Sean) Removes a user string from the blockList.
     //This functions almost the exact same way as removeFriend() btw
-    public void unblockUser(String friendUsername);
+    public boolean unblockUser(String friendUsername);
 
+    //Instead of making it a string I changed it to update each file after the array is changed
     //(Sean) Stringafies the fields of the User class
     //Formatting can be seen in allUsers.txt
-    public String toString();
+//    public String toString();
+    
 }
