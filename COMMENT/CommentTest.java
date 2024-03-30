2543d344550844c893306cc27393065aab33a625
savnimaheshwari <143203323+savnimaheshwari@users.noreply.github.com>
2024-03-30 15:49:25 -0400

    Update CommentTest.java

--- COMMENT/CommentTest.java
+++ COMMENT/CommentTest.java
@@ -1,31 +1,32 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class CommentTest {
 
-    public static void main(String[] args) {
+    @Test(timeout = 1000)
+    public void testComment() {
         // Create a parent post
-        Post parentPost = new Post("user1", "This is a parent post.", 1);
+        Post parentPost1 = new Post("user1", "Hello World!", "post1");
 
-        // Create a comment
-        Comment comment = new Comment("user2", "This is a comment.", 2, parentPost);
+        // Test case for constructor with text
+        Comment comment1 = new Comment("user2", "comment1", "This is a comment", parentPost1);
+        assertEquals("This is a comment", comment1.getText());
+        assertEquals("Hello World!", comment1.getParent().getText());
 
-        // Test getting text
-        System.out.println("Comment text: " + comment.getText());
+        // Test case for constructor with file
+        Post parentPost2 = new Post("user1", "post1");
+        Comment comment2 = new Comment("user2", "comment2", parentPost2);
+        assertEquals("Text of comment2: ", comment2.getText());
+        assertEquals("Parent of comment2: ", comment2.getParent().getText());
 
-        // Test getting parent post
-        System.out.println("Parent post: " + comment.getParent().getText());
+        // Test getParent method
+        assertEquals("Parent of comment1: ", comment1.getParent().getText());
+        assertEquals("Parent of comment2: ", comment2.getParent().getText());
 
-        // Test liking the comment
-        System.out.println("Like status before liking: " + comment.likes("user3")); // Should return false
-        System.out.println("Like status after liking: " + comment.like("user3")); // Should return true
-
-        // Test disliking the comment
-        System.out.println("Dislike status before disliking: " + comment.dislikes("user4")); // Should return false
-        System.out.println("Dislike status after disliking: " + comment.dislike("user4")); // Should return true
-
-        // Test editing the comment
-        comment.editPost("This is an edited comment.");
-        System.out.println("Edited comment text: " + comment.getText());
-
-        // Test deleting the comment
-        System.out.println("Delete status: " + comment.deletePost()); // Should return true
+        // Test likes and dislikes for comments
+        assertTrue(comment1.likes("user1"));
+        assertFalse(comment1.dislikes("user2"));
+        assertFalse(comment2.likes("user1"));
+        assertTrue(comment2.dislikes("user2"));
     }
 }
