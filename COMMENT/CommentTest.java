e2eed528aabde050c349b209c4c203aef50d1e77
fayeteras <eteras@purdue.edu>
2024-03-30 15:16:59 -0400

    Revert "Merge branch 'main' of https://github.com/fayeteras/Team-Project"
    This reverts commit 3291382f63abf0ea372b8ac5ec276214ae7dab6d, reversing
    changes made to 39ebb0e892083d35b30fee3d3d6c5fdfede2d22d.

--- COMMENT/CommentTest.java
+++ COMMENT/CommentTest.java
@@ -1,18 +1,31 @@
-//(savni)
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 public class CommentTest {
 
-    @Test(timeout = 1000)
-    public void testGetParent() {
-        // Set up a parent post
-        Post parentPost = new Post("ParentUser", "ParentText", 1);
-        
-        // Create a comment
-        Comment comment = new Comment("CommentUser", "CommentText", 2, parentPost);
+    public static void main(String[] args) {
+        // Create a parent post
+        Post parentPost = new Post("user1", "This is a parent post.", 1);
 
-        // Test getParent method
-        assertEquals(parentPost, comment.getParent());
+        // Create a comment
+        Comment comment = new Comment("user2", "This is a comment.", 2, parentPost);
+
+        // Test getting text
+        System.out.println("Comment text: " + comment.getText());
+
+        // Test getting parent post
+        System.out.println("Parent post: " + comment.getParent().getText());
+
+        // Test liking the comment
+        System.out.println("Like status before liking: " + comment.likes("user3")); // Should return false
+        System.out.println("Like status after liking: " + comment.like("user3")); // Should return true
+
+        // Test disliking the comment
+        System.out.println("Dislike status before disliking: " + comment.dislikes("user4")); // Should return false
+        System.out.println("Dislike status after disliking: " + comment.dislike("user4")); // Should return true
+
+        // Test editing the comment
+        comment.editPost("This is an edited comment.");
+        System.out.println("Edited comment text: " + comment.getText());
+
+        // Test deleting the comment
+        System.out.println("Delete status: " + comment.deletePost()); // Should return true
     }
 }
--- POST/PostTest.java
+++ POST/PostTest.java
@@ -1,1 +1,26 @@
+//(savni)
+public class PostTest {
 
+    public static void main(String[] args) {
+        // Create a new post
+        Post post = new Post("user123", "This is a test post.", 1);
+
+        // Test getting text
+        System.out.println("Post text: " + post.getText());
+
+        // Test liking the post
+        System.out.println("Like status before liking: " + post.likes("user4")); // Should return false
+        System.out.println("Like status after liking: " + post.like("user4")); // Should return true
+
+        // Test disliking the post
+        System.out.println("Dislike status before disliking: " + post.dislikes("user5")); // Should return false
+        System.out.println("Dislike status after disliking: " + post.dislike("user5")); // Should return true
+
+        // Test editing the post
+        post.editPost("This is an edited post.");
+        System.out.println("Edited post text: " + post.getText());
+
+        // Test deleting the post
+        System.out.println("Delete status: " + post.deletePost()); // Should return true
+    }
+}
