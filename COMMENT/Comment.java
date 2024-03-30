29e6032283c02b670d4699ccb42e6d2f93082e9d
noahwithers <165091315+noahwithers@users.noreply.github.com>
2024-03-30 15:25:45 -0400

    Update Comment.java

--- COMMENT/Comment.java
+++ COMMENT/Comment.java
@@ -2,8 +2,13 @@
 public class Comment extends Post implements CommentInterface {
     private final Post parent;
 
-    public Comment(String username, String text, int postNumber, Post parent) {
-        super(username, text, postNumber);
+    public Comment(String username, String fileName, String text, Post parent) {
+        super(username, text, filename);
+        this.parent = parent;
+    }
+
+    public Comment(String username, String fileName, Post parent) {
+        super(username, fileName);
         this.parent = parent;
     }
 
