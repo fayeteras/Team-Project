62ddbcd0a81428e89afc63c3119ad48e4359144b
savnimaheshwari <143203323+savnimaheshwari@users.noreply.github.com>
2024-03-31 18:06:48 -0400

    Update UserInterface.java

--- USER/UserInterface.java
+++ USER/UserInterface.java
@@ -1,62 +1,41 @@
 import java.io.File;
 import java.util.ArrayList;
-
+/**
+ * UserInterface.java
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2024 -- Team Project
+ *
+ * @author Team 2
+ * @version Mon April 1st, 2024
+ */
 public interface UserInterface {
-    //(Sean) the getters. If there are more variables you think
-    //are needed for the class, don't forget to add the getter.
+    // Method to retrieve the username of the user
     public String getUsername();
-    
+
+    // Method to retrieve the list of friends of the user
     public ArrayList<String> getFriendList();
 
+    // Method to retrieve the list of blocked users of the user
     public ArrayList<String> getBlockList();
 
+    // Method to check existence of the friends file and initialize friendList
     public File checkFriendsFile();
 
+    // Method to check existence of the blocked users file and initialize blockList
     public File checkBlockedFile();
 
-    //(Sean) **FILE USED**
-    //creates a new user by appending the allUsers file
-    //to include the new user.
-    //(Faye) moved to database
-//    public void createUser();
-
-    //(Sean) **FILE USED**
-    //Uses the username parameter to compare to every entry in the
-    //allUsers file, returning the entire user string if found.
-    //Of course the entire user string is too much, but providing too
-    //much is better than too little.
-    //OPTIONAL COMPLEXITY - HW-11 showed how to fetch a line based on a
-    //substring of its content. Maybe this method doesn't require a full username,
-    //rather a substring.. like "fay" grabs "fayeteras"?
-    //Edit: I'm so awesome that I implemented this complexity
-    //Please don't duplicate it on your HW-11 or I will find you.
-    //(Faye) moved to database
-//    public String[] searchUsers(String searchText);
-    
-    //Returns true or false based on whether or not the username and
-    //password parameters match the values of the User object
-//    public boolean authenticateUser(String username, String password);
-
-    //(Faye) I changed all of these to booleans so we can check if there's an error and return an error message
-    //(Sean) adds a friend String to the friendList array in the user class
+    // Method to add a friend to the user's friendList
     public boolean addFriend(String username);
 
-    //(Sean) removes a friend string from the friendList.
+    // Method to remove a friend from the user's friendList
     public boolean removeFriend(String username);
 
-    //(Sean) Adds a user String to the blockList array in the user class
-    //This functions almost the exact same way as addFriend() btw
+    // Method to block a user by adding them to the user's blockList
     public boolean blockUser(String username);
 
-    //(Sean) Removes a user string from the blockList.
-    //This functions almost the exact same way as removeFriend() btw
+    // Method to unblock a user by removing them from the user's blockList
     public boolean unblockUser(String username);
 
-    //Instead of making it a string I changed it to update each file after the array is changed
-    //(Sean) Stringafies the fields of the User class
-    //Formatting can be seen in allUsers.txt
-//    public String toString();
-    
+    // Method to write data to a file
     public boolean writeFile(File filename, ArrayList<String> array);
-    
 }
