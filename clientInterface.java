5f3c77118c7ac1afeb6e1cb356743b525628abce
noahwithers <165091315+noahwithers@users.noreply.github.com>
2024-03-31 09:18:05 -0400

    Update clientInterface.java

--- clientInterface.java
+++ clientInterface.java
@@ -23,9 +23,11 @@ public interface clientInterface {
     public boolean createPost(String postText);
     public boolean likePost(Post post);
     public boolean dislikePost(Post post);
+    public boolean hidePost(Post post);
 
     //Comment Methods - Comment Class
     public boolean createComment(Post post, String commentText);
     public boolean likeComment(Comment comment);
     public boolean dislikeComment(Comment comment);
+    public boolean hideComment(Comment comment);
 }
