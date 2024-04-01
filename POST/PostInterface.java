0b5a6239b2b64ff67753f62ceac8fcaa8919ef68
Sean VandenBussche <165090862+Seanski05@users.noreply.github.com>
2024-04-01 13:52:31 -0400

    javadoc added + unnecessary comments removed

--- POST/PostInterface.java
+++ POST/PostInterface.java
@@ -1,9 +1,17 @@
-import java.util.ArrayList;
+import java.util.*;
+
+/**
+ * PostInterface.java
+ *
+ * This is an interface used to call methods in the Post class.
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2024 -- Team Project
+ *
+ * @author LO4-Team 2
+ * @version Sat March 30th, 2024
+ */
 
 public interface PostInterface {
-    //(Savni) Creates a post that contains a string of content
-    //public void makePost(String content);
-
     //(Savni) Takes a post and increments the like total of it
     public boolean like(String username);
 
@@ -21,23 +29,17 @@ public interface PostInterface {
     public boolean dislike(String username);
     public String getText();
 
-    //(Savni) Takes a post and hides it from the user's feed
-    //public void hidePost(Post post);
-
     //(Savni) Takes a post and deletes it
     //Note that this should only be done by the post owner
     public boolean deletePost();
-    public boolean hide(String username);
 
-    //(Sean) Stringafies the fields of the Post class
-    //Formatting can be seen in userPosts.txt
-    //public String toString();
+    //(Savni) hides posts
+    public boolean hide(String username);
 
     //(Faye) gets current time stamp (method written by Tyler)
     public int[] getCurrentTime();
 
     //(Faye) replaces old comment with new text (method written by Noah)
     public void editPost(String newText);
-
 
 }
