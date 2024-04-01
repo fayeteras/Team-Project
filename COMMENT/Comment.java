e74c4a54573783763712454b77ffca9f77f80a58
Sean VandenBussche <165090862+Seanski05@users.noreply.github.com>
2024-04-01 12:20:21 -0400

    javadoc added.

--- COMMENT/Comment.java
+++ COMMENT/Comment.java
@@ -1,4 +1,14 @@
-//(Noah) marked as i made this 
+/**
+ * Comment.java
+ *
+ * The child class of Post.java. Similar to a Post Object, except 
+ * a Comment Object has an additional field to store the parent post for that comment.
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2024 -- Team Project
+ *
+ * @author LO4-Team 2 - Noah Withers
+ * @version Sat March 30th, 2024
+ */
 public class Comment extends Post implements CommentInterface {
     private final Post parent;
 
