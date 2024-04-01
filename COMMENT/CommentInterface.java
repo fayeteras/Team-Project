23816accfe0ff7f1005c5c232247dea8dd1f608f
Sean VandenBussche <165090862+Seanski05@users.noreply.github.com>
2024-04-01 12:24:28 -0400

    javadoc added + unnecessary comments removed

--- COMMENT/CommentInterface.java
+++ COMMENT/CommentInterface.java
@@ -1,24 +1,15 @@
+/**
+ * CommentInterface.java
+ *
+ * This is an interface used to call methods in the Comment class.
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2024 -- Team Project
+ *
+ * @author LO4-Team 2 - Noah Withers
+ * @version Fri March 29th, 2024
+ */
 public interface CommentInterface {
-    /*
-    //(Savni) Takes a comment from the user and adds it to the post specified by the user
-    public void addComment(Post post, Comment comment);
 
-    //(Savni) Takes a comment from the user and deletes it from the post specified by the user
-    //Be aware that only a post owner or comment owner can do this
-    public void deleteComment(Post post, Comment comment);
-
-    //(Savni) Takes a comment and increments the likes of it
-    public void likeComment(Comment comment);
-
-    //(Savni) Takes a comment and increments the dislikes of it
-    public void dislikeComment(Comment comment);
-
-    //(Sean) Stringafies the fields of the Comment class
-    //Formatting can be seen in userComments.txt
-    public String toString();
-    */
-
-    //(Noah) Comment is a child of post, so these would be inherited. That or they don't make sense. I added the only one in the comment class rn below.
-
+    //(Noah) Comment is a child of post, so there is only one new method that is not inherited
     public Post getParent();
 }
