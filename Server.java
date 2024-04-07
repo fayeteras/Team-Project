92bd8aaae638fcb4ea9840ac60ebf5c532790bd9
Sean VandenBussche <165090862+Seanski05@users.noreply.github.com>
2024-04-07 14:49:05 -0400

    Friday's Meeting Code

--- Server.java
+++ Server.java
@@ -1,26 +1,62 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server implements Runnable {
 
-    private static final int PORT = 0;
+    private static final int PORT = 620;
+    private final Socket socket;
 
-    public static void main(String[] args) {
+    public Server(Socket socket) {
+        this.socket = socket;
+    }
+
+    public User createUser(String username) {
+        User result = new User("sugon.txt");
+        return result;
+    }
+    @Override
+    public void run() {
         try {
-            ServerSocket serverSocket = new ServerSocket(PORT);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            while (true) {
+                String command = reader.readLine();
+                //you are here
+                switch (command) {
+                    case "login": String username;
+                    String bean = reader.readLine();
+
+                        break;
+                    case "logout":
+                        break;
+                    case "oh":
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Server started and listening on port " + PORT);
             while (true) {
                 Socket clientSocket = serverSocket.accept();
+                Server server = new Server(clientSocket);
+                new Thread(server).start();
             }
-        
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 
-    @Override
-    public void run() {
-        
-    }
-    
-
-    
 }
