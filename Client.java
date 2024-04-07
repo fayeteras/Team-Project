ba9f6a127032b869182126843fda708e6e661b4b
Sean VandenBussche <165090862+Seanski05@users.noreply.github.com>
2024-04-07 14:47:26 -0400

    Friday's Meeting code

--- Client.java
+++ Client.java
@@ -1,3 +1,23 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Client {
-    
+    Scanner scan = new Scanner(System.in);
+    static String host = "localhost";
+    static int port = 620;
+    public static void main(String[] args) {
+        try (Socket socket = new Socket(host, port)) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            //Forward-thinking is strongly encouraged
+            //you WILL add a GUI
+            //you WILL redo the client
+            //you WILL code sober
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
