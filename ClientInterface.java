eb94a1e247b312ff8654e696eaf6a5b6dca4d2f1
Sean VandenBussche <165090862+Seanski05@users.noreply.github.com>
2024-04-15 14:39:58 -0400

    updated all method parameters + renamed file

---
+++ ClientInterface.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+/**
+ * ClientInterface.java
+ *
+ * This interface is for Client.java. Outlines the methods to be used in the Client class.
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2024 -- Team Project
+ *
+ * @author LO4-Team 2
+ * @version Sun Mar 31st, 2024
+ */
+public interface ClientInterface {
+    public void createUser(Scanner scan, BufferedReader reader, PrintWriter writer);
+
+    //Sign in -- will authenticate through database and create a new serversocket for the user
+    public void signIn(Scanner scan, BufferedReader reader, PrintWriter writer);
+
+    //Search Methods
+    public void userSearch(Scanner scan, BufferedReader reader, PrintWriter writer);
+
+    //User Methods - sends username to server
+    public void viewProfile(Scanner scan, BufferedReader reader, PrintWriter writer);
+    public boolean friendUser(Scanner scan, BufferedReader reader, PrintWriter writer);
+    public boolean unfriendUser(Scanner scan, BufferedReader reader, PrintWriter writer);
+    public boolean blockUser(Scanner scan, BufferedReader reader, PrintWriter writer);
+    public boolean unblockUser(Scanner scan, BufferedReader reader, PrintWriter writer);
+
+    //Feed Methods
+    public boolean getFeed(Scanner scan, BufferedReader reader, PrintWriter writer);
+
+    //Post Methods - Post Class
+    public boolean createPost(Scanner scan, BufferedReader reader, PrintWriter writer);
+
+    //Comment Methods - Comment Class
+    public boolean createComment(Scanner scan, BufferedReader reader, PrintWriter writer);
+
+    //Comment + Post interaction methods
+    public boolean like(Scanner scan, BufferedReader reader, PrintWriter writer);
+    public boolean dislike(Scanner scan, BufferedReader reader, PrintWriter writer);
+    public boolean hide(Scanner scan, BufferedReader reader, PrintWriter writer);
+    public boolean edit(Scanner scan, BufferedReader reader, PrintWriter writer);
+
+}
