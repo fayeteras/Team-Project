6580acd8ce355fa246190c88d8abac3f56a73b69
Sean VandenBussche <165090862+Seanski05@users.noreply.github.com>
2024-04-26 16:33:28 -0400

    re-added signin and signup message changes

--- Server.java
+++ Server.java
@@ -152,7 +152,7 @@ public class Server implements Runnable, ServerInterface {
                         userArray.add(newUser);
                         user = newUser;
                         yesOrNo = "No";
-                        writer.write("Account creation successful. Welcome " + username + "!");
+                        writer.write("Account created. Welcome " + username + "! Loading our platform now...");
                         writer.println();
                         writer.flush();
                     } else {
@@ -185,7 +185,7 @@ public class Server implements Runnable, ServerInterface {
                 password = reader.readLine();
                 if (db.authenticateUser(username, password)) {
                     yesOrNo = "No";
-                    writer.write("Login successful. Welcome " + username + "!");
+                    writer.write("Login successful. Welcome " + username + "! Loading our platform now...");
                     user = new User(username);
                     writer.println();
                     writer.flush();
