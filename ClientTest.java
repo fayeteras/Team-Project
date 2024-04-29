461543c182cfbd4d43e99c41af29b9c2e9ec12e5
Sean VandenBussche <165090862+Seanski05@users.noreply.github.com>
2024-04-29 15:29:52 -0400

    Edited tests that no longer worked due to gui changes

--- ClientTest.java
+++ ClientTest.java
@@ -14,148 +14,13 @@ import org.junit.*;
  * @version Sun Apr 14th, 2024
  */
 public class ClientTest {
-    //(Sean) Code from a previous HW test case that helped me with the inputs and outputs
-    private final PrintStream originalOutput = System.out;
-    private final InputStream originalSysin = System.in;
-    @SuppressWarnings("FieldCanBeLocal")
-    private ByteArrayInputStream testIn;
-    @SuppressWarnings("FieldCanBeLocal")
-    private ByteArrayOutputStream testOut;
-    @Before
-    public void outputStart() {
-        testOut = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(testOut));
-    }
-    @After
-    public void restoreInputAndOutput() {
-        System.setIn(originalSysin);
-        System.setOut(originalOutput);
-    }
-    private String getOutput() {
-        return testOut.toString();
-    }
-    @SuppressWarnings("SameParameterValue")
-    private void receiveInput(String str) {
-        testIn = new ByteArrayInputStream(str.getBytes());
-        System.setIn(testIn);
-    }
-
-    //(Sean) Each of the correct outputs for my methods
-    public static final String WELCOME_MESSAGE = "What would you like to do, User?";
-    public static final String ENTER_USERNAME_ONE = "Please enter a username.";
-    public static final String ENTER_USERNAME_TWO = "Please enter your username.";
-    public static final String ENTER_PASSWORD_ONE = "Please enter a password.";
-    public static final String ENTER_PASSWORD_TWO = "Please enter your password.";
-    public static final String LOGIN_SUCCESS = "Login successful. Welcome ";
-    public static final String ACCOUNT_SUCCESS = "Account creation successful. Welcome ";
-    public static final String USER_SEARCH_PROMPT = "Please enter the username of who you're looking for.";
-    public static final String USER_SEARCH_SUCCESS = "Here is the User you are looking for.";
-
-    @Test(timeout = 1000)
-    public void testCreateUser() {
-        // Create a mock BufferedReader with expected server response
-        String serverResponse = WELCOME_MESSAGE + System.lineSeparator() + ENTER_USERNAME_ONE +
-                System.lineSeparator() + ENTER_PASSWORD_ONE + System.lineSeparator() +
-                ACCOUNT_SUCCESS + "testUser!" + System.lineSeparator();
-        BufferedReader mockReader = new BufferedReader(new StringReader(serverResponse));
-
-        // Create a StringWriter to capture client output
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter mockWriter = new PrintWriter(stringWriter);
-
-        //Create a mock Scanner to scan the test input;
-        try {
-            Client testClient = new Client();
-            String input = "testUser" + System.lineSeparator() + "testPassword" + System.lineSeparator()
-                    + "No" + System.lineSeparator();
-            Scanner mockScanner = new Scanner(input);
-            // Call the createUser method
-            testClient.createUser(mockScanner, mockReader, mockWriter);
-            String output = getOutput();
-
-            // Verify the output matches the expected output
-            assertEquals(serverResponse, output);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test(timeout = 1000)
-    public void testSignIn() {
-        // Create a mock BufferedReader with expected server response
-        //The createUser method is first, then the signIn method follows to ensure that
-        //the test can sign in with this newly created User.
-        String serverResponse = WELCOME_MESSAGE + System.lineSeparator() + ENTER_USERNAME_ONE +
-                System.lineSeparator() + ENTER_PASSWORD_ONE + System.lineSeparator() +
-                ACCOUNT_SUCCESS + "testUser!" + System.lineSeparator() +
-                WELCOME_MESSAGE + System.lineSeparator() + ENTER_USERNAME_TWO +
-                System.lineSeparator() + ENTER_PASSWORD_TWO + System.lineSeparator() +
-                LOGIN_SUCCESS + "testUser!" + System.lineSeparator();
-        BufferedReader mockReader = new BufferedReader(new StringReader(serverResponse));
-
-        // Create a StringWriter to capture client output
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter mockWriter = new PrintWriter(stringWriter);
-
-        //Create a mock Scanner to scan the test input;
-        try {
-            Client testClient = new Client();
-            String input = "testUser" + System.lineSeparator() + "testPassword" + System.lineSeparator()
-                    + "No" + System.lineSeparator() + "testUser" + System.lineSeparator()
-                    + "testPassword" + System.lineSeparator() + "No" + System.lineSeparator();
-            Scanner mockScanner = new Scanner(input);
-            // Call the createUser and signIn method
-            testClient.createUser(mockScanner, mockReader, mockWriter);
-            testClient.signIn(mockScanner, mockReader, mockWriter);
-            String output = getOutput();
-
-            // Verify the output matches the expected output
-            assertEquals(serverResponse, output);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test(timeout = 1000)
-    public void testUserSearch() {
-        // Create a mock BufferedReader with expected server response
-        //The createUser method is first, then the userSearch method follows to ensure that
-        //the test can search up the newly created User.
-        String serverResponse = WELCOME_MESSAGE + System.lineSeparator() + ENTER_USERNAME_ONE +
-                System.lineSeparator() + ENTER_PASSWORD_ONE + System.lineSeparator() +
-                ACCOUNT_SUCCESS + "testUser!" + System.lineSeparator() +
-                WELCOME_MESSAGE + System.lineSeparator() + USER_SEARCH_PROMPT +
-                System.lineSeparator() + USER_SEARCH_SUCCESS + System.lineSeparator();
-        BufferedReader mockReader = new BufferedReader(new StringReader(serverResponse));
-
-        // Create a StringWriter to capture client output
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter mockWriter = new PrintWriter(stringWriter);
-
-        //Create a mock Scanner to scan the test input;
-        try {
-            Client testClient = new Client();
-            String input = "testUser" + System.lineSeparator() + "testPassword" + System.lineSeparator()
-                    + "No" + System.lineSeparator() + "testUser" + System.lineSeparator()
-                    + "No" + System.lineSeparator();
-            Scanner mockScanner = new Scanner(input);
-            // Call the createUser and userSearch method
-            testClient.createUser(mockScanner, mockReader, mockWriter);
-            testClient.userSearch(mockScanner, mockReader, mockWriter);
-            String output = getOutput();
-
-            // Verify the output matches the expected output
-            assertEquals(serverResponse, output);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     @Test(timeout = 1000)
     public void testGetFeed() {
         // Create a mock BufferedReader with server response
         String serverResponse = "get feed" + System.lineSeparator();
         BufferedReader mockReader = new BufferedReader(new StringReader(serverResponse));
+        String expectedFeed[] = new String[1];
+        expectedFeed[0] = "testPost";
 
         // Create a StringWriter to capture client output
         StringWriter stringWriter = new StringWriter();
@@ -164,8 +29,12 @@ public class ClientTest {
         try {
             Client testClient = new Client();
             // Call the getFeed method
+            ArrayList<String[]> result = testClient.getFeed(null, mockReader, mockWriter);
 
             // Verify that the method returns true (indicating success)
+            for (int i = 0; i < result.size(); i++) {
+                assertTrue(result.get(i).equals(expectedFeed));
+            }
 
             // Verify the output matches the expected output
             assertEquals(serverResponse, stringWriter.toString());
@@ -183,6 +52,8 @@ public class ClientTest {
         BufferedReader br = new BufferedReader(new StringReader("True"));
         try {
             Client testClient = new Client();
+            testClient.setWriter(writer);
+            testClient.setReader(br);
             boolean tester = testClient.createPost("testUser", "text");
             assertTrue(tester);
         } catch (Exception e) {
@@ -264,7 +135,8 @@ public class ClientTest {
         try {
             Client testClient = new Client();
             boolean tester = testClient.createPost("testUser", "text");
-            assertTrue(tester);
+            //Assets that the improperly formatted comment cannot be created
+            assertFalse(tester);
         } catch (Exception e) {
             e.printStackTrace();
         }
